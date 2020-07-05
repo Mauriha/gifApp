0204de72bcc1486197db92172f1ce59ff0929beb
Fer <dr.mauriha@gmail.com>
2020-07-05 12:25:26 -0400

    cambiado el nombre de la carpeta  "build" a "docs"

---
+++ docs/precache-manifest.637130445259775e037d7d3d2b022968.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73d93376ed4f4212aa350723b539c4de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fd506e8f4a6e0af654fc",
+    "url": "/static/css/2.e2dd9b0a.chunk.css"
+  },
+  {
+    "revision": "8eea7d41e1c923f0aca4",
+    "url": "/static/css/main.194d4895.chunk.css"
+  },
+  {
+    "revision": "fd506e8f4a6e0af654fc",
+    "url": "/static/js/2.6e92036a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6e92036a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8eea7d41e1c923f0aca4",
+    "url": "/static/js/main.8be73b35.chunk.js"
+  },
+  {
+    "revision": "b51bde8fbf36acf6249c",
+    "url": "/static/js/runtime-main.401d87cc.js"
+  }
+]);
